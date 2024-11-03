a171e48e3ba9723658c45f9acf0f69b91526acc1
vselected <dominik.sarleja@gmail.com>
2024-11-03 18:58:04 +0100

    Added comments and Excercice

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-
+#Create Variable named "string" with value "Hello"
 string = "Hello"
+
+#Printing indexing examples
 print(string[0])
 print(string[:4])
 print(string[-1])
 
+#Create Variable named "string_alphabet" with value "abcdefghijk"
+string_alphabet = "abcdefghijk"
 
+#Printing indexing examples
+print(string_alphabet)
+print(string_alphabet[2:])
+print(string_alphabet[:3])
+print(string_alphabet[3:6])
+print(string_alphabet[1:3])
+
+#Excercice try to get only "defghi" from variable string_alphabet
+print(string_alphabet[3:9])
 
