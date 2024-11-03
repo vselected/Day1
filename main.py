8aedb7e9bf1a96fbcd7dbf3afd13b8536e3704db
vselected <dominik.sarleja@gmail.com>
2024-11-03 19:08:15 +0100

    Exercises and start to learn about String Properties and Methods

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-#Create Variable named "string" with value "Hello"
+# Create Variable named "string" with value "Hello"
 string = "Hello"
 
-#Printing indexing examples
+# Printing indexing examples
 print(string[0])
 print(string[:4])
 print(string[-1])
 
-#Create Variable named "string_alphabet" with value "abcdefghijk"
+# Create Variable named "string_alphabet" with value "abcdefghijk"
 string_alphabet = "abcdefghijk"
 
-#Printing indexing examples
+# Printing indexing examples
 print(string_alphabet)
 print(string_alphabet[2:])
 print(string_alphabet[:3])
 print(string_alphabet[3:6])
 print(string_alphabet[1:3])
 
-#Excercice try to get only "defghi" from variable string_alphabet
+# Exercise try to get only "defghi" from variable string_alphabet
 print(string_alphabet[3:9])
 
+# Printing slicing examples
+print(string_alphabet[::2])
+print(string_alphabet[2:7:2])
+print(string_alphabet[::-1])
+
+# Exercise 1
+# Write a string index that returns just the letter 'r'  from 'Hello World' .
+# For example, 'Hello World'[0]  returns 'H'
+# You should only write one line of code for this. Do not assign a variable name to the string.
+print("Hello World"[8])
+
+# Exercise 2
+# String Slicing
+# Use string slicing to grab the word 'ink'  from inside 'tinker'
+# For example, 'education'[3:6]  returns 'cat'
+# Remember that when slicing you only go up to but not including the end index.
+# You should only write one line of code for this. Do not assign a variable name to the string.
+print("tinker"[1:4])
+
+#Learning about String Properties and Methods
